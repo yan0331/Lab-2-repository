8699d7034d40c6f71ed7cd07450f464b28c1840b
yan0331 <156141947+yan0331@users.noreply.github.com>
2024-02-12 19:33:21 -0500

    Create lab2.py

---
+++ lab2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+
+def function():
+    script_name = sys.argv[0]
+    variables = sys.argv[1:]
+    
+    print("Script:", script_name)
+    print("Script AND Variables:", script_name, variables)
+
+function()
